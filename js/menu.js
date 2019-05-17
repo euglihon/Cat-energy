c2dd7816ff6964be5b7c178fceab8298e64f7e8b
Eugene Lihon <eug.lihon@hotmail.com>
2019-05-17 19:35:56 +0200

    Updates

--- js/menu.js
+++ js/menu.js
@@ -1,6 +1,9 @@
 var MainMenuBtn = document.querySelector(".pages-header__togle");
 var MainMenuList = document.querySelector(".main-nav");
 
+
+MainMenuList.classList.remove("main-nav--no-js");
+
 MainMenuBtn.addEventListener("click", function(evt) {
   evt.preventDefault();
   MainMenuList.classList.toggle("main-nav--open");
